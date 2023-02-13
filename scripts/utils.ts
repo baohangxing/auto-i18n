0b2965cf79de1c10e166b0b756e82601f6329b75
baohangxing <2292398086@qq.com>
2023-02-13 17:44:14 +0800

    chore: release v0.2.0

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -15,7 +15,7 @@ const updataReadme = (readmePath: string) => {
     = '> 请阅读[gitlab上的文档](https://gitcn.yostar.net:8888/hangxing.bao/yo-auto-i18n#%E4%BB%8B%E7%BB%8D)'
     + `以获取更好的阅读体验\n\r\n\r${readFileSync(path.resolve('./README.md')).toString()}`
 
-  writeFileSync(path.resolve(readmePath, './README.md'), readme)
+  writeFileSync(path.resolve(readmePath, './README.md'), readme, 'utf8')
 }
 
 export { changePackageVersion, updataReadme }
